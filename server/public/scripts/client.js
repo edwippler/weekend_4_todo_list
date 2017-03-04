ea4590c01ed0c6ebd3466df43ac5a0b7582021b4
Edmund Wippler <ed.wippler@gmail.com>
2017-03-04 17:32:46 -0600

    set-up index files, folder structure, and dependencies

---
+++ server/public/scripts/client.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  console.log('Sorcery, hell yeah!');
+
+// collect value of added task to append DOM and database
+  $('#addButton').on('click', function(){
+    var newTask = $('#newTask').val();
+    console.log(newTask);
+  }); //end of addtask function
+
+
+//
+
+
+});
